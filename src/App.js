af72c77052d9fff70967a9032983214b53792dde
eveligora <eveligora@gmail.com>
2022-06-03 10:22:01 +0300

    favicon.ico

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import logo from "./logo.svg";
-import "./App.scss";
 import "../node_modules/bootstrap/scss/bootstrap.scss";
+import "./scss/reset.scss";
+import "./scss/main.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link btn btn-danger"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Companies />
     </div>
   );
-}
+};
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__in">
+        <div className="logo_w">
+          <img src={logo} className="logo" alt="logo" />
+        </div>
+        <nav className="header__menu_w">
+          <ul className="menu">
+            <li className="menu__item">
+              <a href="/login" className="menu__link">
+                Login
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href="/companies" className="menu__link">
+                Companies
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+const Companies = () => {
+  return (
+    <div>
+      <div className="container">Companies</div>;
+    </div>
+  );
+};
 
 export default App;
